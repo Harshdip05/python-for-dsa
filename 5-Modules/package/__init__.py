a27c65f1d895636c84d0efee811631ee5cfb7962
Harshdip05 <harshdipnandre05@gmail.com>
2026-03-03 09:53:08 +0530

    Define custom module or package

---
+++ 5-Modules/package/__init__.py
@@ -0,0 +1,11 @@
+''' 
+
+__init__.py is a special file used in python to define 
+packages and initialize their namespaces
+
+'''
+
+
+
+# this package folder.math becomes our module since we our using __init__.py
+# if initialize folder and define packages or modules we need __init__.py
---
+++ 5-Modules/package/maths.py
@@ -0,0 +1,10 @@
+# define a function
+
+def addition(a,b):
+    return a+b
+
+
+# 
+
+def sub(a,b):
+    return a-b
